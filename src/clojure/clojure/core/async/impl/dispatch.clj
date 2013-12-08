083232cefe5458ab31b1b5a6028377f96263c741
David Miller <dmiller2718@gmail.com>
2013-12-08 17:45:34 -0600

    Move dispatch.clj to BoundedTaskScheduler

--- src/clojure/clojure/core/async/impl/dispatch.clj
+++ src/clojure/clojure/core/async/impl/dispatch.clj
@@ -6,22 +6,18 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 ;;;  Ported to ClojureCLR by David Miller
-
-(assembly-load-from "TaskExtensions.dll")
-
+(assembly-load-from "clojure.core.async.util.BoundedTaskScheduler.dll")
 (ns ^{:skip-wiki true}
   clojure.core.async.impl.dispatch
-  (:import [System.Threading.Tasks.Schedulers  LimitedConcurrencyLevelTaskScheduler]      ;;; DM:Added
-           [System.Threading.Tasks             TaskFactory])                             ;;; DM:Added
   (:require [clojure.core.async.impl.protocols :as impl]
-			[clojure.core.async.impl.concurrent :as conc])                               ;;; DM:Added  
+			[clojure.core.async.impl.concurrent :as conc])                  ;;; DM:Added  
+  (:import [clojure.core.async.util BoundedTaskScheduler]                   ;;; DM:Added
+           [System.Threading.Tasks             TaskFactory])                ;;; DM:Added
             )                                                ;;; [clojure.core.async.impl.exec.threadpool :as tp]
 
 (set! *warn-on-reflection* true)
 
 ;;; We don't have fun thread-pool goodies found in the JVM.
-;;; I have taken the LimitedConcurrencyLevelTaskScheduler from the Microsoft Samples for Parallel Programming to 
-;;; get the desired, well, limited concurrency.
 ;;; I have not ported impl/exec/threadpool -- I'll just do the work of creating the custom scheduler here.
 ;;; I have not ported impl/concurrent, except for the processor count 
 ;;;     -- I don't see a hook in the CLR to modify the thread names in the pool.
@@ -34,7 +30,7 @@
 ;;;  [^Runnable r]
 ;;;  (impl/exec @executor r))
 
-(def task-factory (delay (TaskFactory. (LimitedConcurrencyLevelTaskScheduler.  (+ 2 conc/processors)))))
+(def task-factory (delay (TaskFactory. (BoundedTaskScheduler.  (+ 2 conc/processors)))))
 
 (defn run
   "Runs fn in thread pool thread"
