510071ab93bb4573850f201a63c46d1dc2723424
David Miller <dmiller2718@gmail.com>
2013-12-09 21:43:09 -0600

    Add C# class ThreadLocalRandom

---
+++ src/csharp/Utilities/Utilities/ThreadLocalRandom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace clojure.core.async
+{
+    public class ThreadLocalRandom : Random
+    {
+        static private ThreadLocal<ThreadLocalRandom> _currentThreadLocalRandom = new ThreadLocal<ThreadLocalRandom>(() =>
+            {
+                return new ThreadLocalRandom();
+            });
+
+        public static ThreadLocalRandom current { get { return _currentThreadLocalRandom.Value; } }
+        public static ThreadLocalRandom Current { get { return _currentThreadLocalRandom.Value; } }
+
+
+        private ThreadLocalRandom()
+            : base()
+        {
+        }
+
+        private ThreadLocalRandom(int seed)
+            : base(seed)
+        {
+        }
+
+
+    }
+}
